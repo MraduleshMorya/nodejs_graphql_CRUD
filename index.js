8050abce7466d1f3e589a2f6797b37280143f3e9
Mradulesh Morya <76241036+MraduleshMorya@users.noreply.github.com>
2022-09-15 12:48:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { express_obj } = require("./app")
 
-express_obj.listen(8000,'192.168.50.67',()=>{
+express_obj.listen(8000,()=>{
     console.log("Server Up and Running on http://192.168.50.67:8000");
     console.log("Listening for requests :-");
 })
